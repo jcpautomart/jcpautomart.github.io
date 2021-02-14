144414b4fc3ef03093fe646fb96bea659c9f53e9
Jayamaha <jcpautomart@gmail.com>
2021-02-14 23:19:50 +0530

    Adding simple JS

---
+++ basic-script.js
@@ -0,0 +1,8 @@
+// Basic scripts for the page
+$(document).ready(() => {
+  $("#writeQuote").hide();
+
+  $("button").click(() => {
+    window.open('https://forms.gle/DEuRqiKTN76ZXgX47', '_blank');
+  });
+});
